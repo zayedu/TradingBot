e9b9e854af2351d9cbb278e52401763bb0bb63d6
zayedu <umerzayed1@gmail.com>
2024-06-08 18:06:29 -0400

    feat: add gitignore

--- tradingbot.py
+++ tradingbot.py
@@ -1,2 +1,14 @@
 from lumibot.brokers import Alpaca
-from lumibot.backtesting import YahooDataBackTesting
+from lumibot.backtesting import YahooDataBacktesting
+from lumibot.strategies.strategy import Strategy
+from lumibot.traders import Trader
+from datetime import datetime
+
+API_KEY = ''
+API_SECRET = ''
+BASE_URL=''
+ALPACA_CREDS = {
+    'API_KEY': API_KEY,
+    'API_SECRET': API_SECRET,
+    'PAPER': True
+}
