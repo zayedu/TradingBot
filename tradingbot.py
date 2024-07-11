a6eb74bf9b7257ed82d81b74e1012a4e025f7b25
zayedu <umerzayed1@gmail.com>
2024-07-10 20:08:27 -0400

    feat: set up backtesting

--- tradingbot.py
+++ tradingbot.py
@@ -3,12 +3,50 @@ from lumibot.backtesting import YahooDataBacktesting
 from lumibot.strategies.strategy import Strategy
 from lumibot.traders import Trader
 from datetime import datetime
+import Secrets
 
-API_KEY = ''
-API_SECRET = ''
-BASE_URL=''
+API_KEY = Secrets.API_KEY
+API_SECRET = Secrets.API_SECRET
+BASE_URL = Secrets.BASE_URL
+
 ALPACA_CREDS = {
     'API_KEY': API_KEY,
     'API_SECRET': API_SECRET,
     'PAPER': True
 }
+
+
+class MLTrader(Strategy):
+    def initialize(self, symbol: str ="SPY"):
+        self.symbol = symbol
+        self.sleeptime = "24H"
+        self.last_trade = None
+
+    def position_sizing(self):
+        cash = self.get_cash()
+        last_price = self.get_last_price(self.symbol)
+
+    def on_trading_iteration(self):
+        if self.last_trade is None:
+            order = self.create_order(
+                self.symbol,
+                10,
+                "buy",
+                type="market"
+            )
+            self.submit_order(order)
+            self.last_trade = "buy"
+
+start_date = datetime(2023, 12,15)
+end_date = datetime(2023, 12, 31)
+
+broker = Alpaca(ALPACA_CREDS)
+strategy = MLTrader(name = 'mlstrat', broker = broker, parameters = {"symbol": "SPY"})
+strategy.backtest(
+    YahooDataBacktesting,
+    start_date,
+    end_date
+)
+
+
+
