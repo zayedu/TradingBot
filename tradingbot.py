028745a8590b26e04ccdf1982492bf8ce3e86042
zayedu <umerzayed1@gmail.com>
2024-07-10 21:43:11 -0400

    feat: implement sentiment in to decision making

--- tradingbot.py
+++ tradingbot.py
@@ -5,6 +5,7 @@ from lumibot.traders import Trader
 from datetime import datetime
 from alpaca_trade_api import REST
 from timedelta import Timedelta
+from finbert_utils import estimate_sentiment
 
 import Secrets
 
@@ -20,12 +21,12 @@ ALPACA_CREDS = {
 
 
 class MLTrader(Strategy):
-    def initialize(self, symbol: str ="SPY", cash_at_risk:float = 0.5):
+    def initialize(self, symbol: str = "SPY", cash_at_risk: float = 0.5):
         self.symbol = symbol
         self.sleeptime = "24H"
         self.last_trade = None
         self.cash_at_risk = cash_at_risk
-        self.api = REST(key_id = API_KEY, secret_key = API_SECRET, base_url = BASE_URL)
+        self.api = REST(key_id=API_KEY, secret_key=API_SECRET, base_url=BASE_URL)
 
     def position_sizing(self):
         cash = self.get_cash()
@@ -35,18 +36,24 @@ class MLTrader(Strategy):
 
     def get_dates(self):
         today = self.get_datetime()
-        three_days_prior = today - Timedelta(days = 3)
+        three_days_prior = today - Timedelta(days=3)
         return today.strftime("%Y-%m-%d"), three_days_prior.strftime("%Y-%m-%d")
+
     def get_sentiment(self):
-        today,three_days_prior = self.get_dates()
-        news = self.api.get_news(symbol = self.symbol, start = three_days_prior , end = today)
+        today, three_days_prior = self.get_dates()
+        news = self.api.get_news(symbol=self.symbol, start=three_days_prior, end=today)
         headlines = [news_item.__dict__['_raw']['headline'] for news_item in news]
-        return headlines
+        probability, sentiment = estimate_sentiment(headlines)
+
+        return probability, sentiment
+
     def on_trading_iteration(self):
         cash, last_price, quantity = self.position_sizing()
+        probability, sentiment = self.get_sentiment()
         if cash > last_price:
-            if self.last_trade is None:
-                news = self.get_news()
+            if sentiment == "positive" and probability > 0.999:
+                if self.last_trade == "sell":
+                    self.sell_all()
                 order = self.create_order(
                     self.symbol,
                     quantity,
@@ -58,20 +65,30 @@ class MLTrader(Strategy):
                 self.submit_order(order)
                 self.last_trade = "buy"
 
+            elif sentiment == "negative" and probability > 0.999:
+                if self.last_trade == "buy":
+                    self.sell_all()
+                order = self.create_order(
+                    self.symbol,
+                    quantity,
+                    "sell",
+                    type="bracket",
+                    take_profit_price=last_price * 0.8,
+                    stop_loss_price=last_price * 1.05
+                )
+                self.submit_order(order)
+                self.last_trade = "sell"
 
 
-start_date = datetime(2023, 12,15)
+start_date = datetime(2020, 1, 1)
 end_date = datetime(2023, 12, 31)
 
 broker = Alpaca(ALPACA_CREDS)
-strategy = MLTrader(name = 'mlstrat', broker = broker,
-                    parameters = {"symbol": "SPY",
-                                  "cash_at_risk":0.5})
+strategy = MLTrader(name='mlstrat', broker=broker,
+                    parameters={"symbol": "SPY",
+                                "cash_at_risk": 0.5})
 strategy.backtest(
     YahooDataBacktesting,
     start_date,
     end_date
 )
-
-
-
