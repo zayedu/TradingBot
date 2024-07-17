d87d540062dff91eabbc838b3a5cfcbaa21fa1aa
zayedu <umerzayed1@gmail.com>
2024-07-16 23:22:52 -0400

    fix: fix sheet rendering exceptions

--- app.py
+++ app.py
@@ -32,7 +32,6 @@ def backtrade(start_date, end_date):
 
     p.join()
 
-
     # Get a list of all .html files in the 'algorithm/logs' directory
     files = glob.glob('logs/*.html')
 
@@ -40,20 +39,19 @@ def backtrade(start_date, end_date):
     files.sort(key=os.path.getmtime, reverse=True)
 
     # Get the two most recently modified files
-    recent_file1 = files[0] if len(files) > 0 else None
-    recent_file2 = files[1] if len(files) > 1 else None
+    recent_file1 = files[0] if len(files) > 0 else 'default.html'
+    recent_file2 = files[1] if len(files) > 1 else 'default.html'
 
     # Move the files to the static directory and get the new file paths
-    if recent_file1:
+    if recent_file1 != 'default.html':
         os.rename(recent_file1, 'static/' + os.path.basename(recent_file1))
         recent_file1 = os.path.basename(recent_file1)
 
-    if recent_file2:
+    if recent_file2 != 'default.html':
         os.rename(recent_file2, 'static/' + os.path.basename(recent_file2))
         recent_file2 = os.path.basename(recent_file2)
 
     return render_template('backtrade.html', file1=recent_file1, file2=recent_file2)
-
 
 if __name__ == '__main__':
     app.run(threaded=False)
